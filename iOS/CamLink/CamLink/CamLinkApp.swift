8d5e4ab41a542d80859fdc7a6dcc375617f1ea70
Akshay Vaswani <34140914+AkshayVaswani@users.noreply.github.com>
2024-02-22 21:26:49 -0500

    init xCode project

---
+++ iOS/CamLink/CamLink/CamLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamLinkApp.swift
+//  CamLink
+//
+//  Created by Akshay Vaswani on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct CamLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
